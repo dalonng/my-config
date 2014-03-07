552f63b5e17dd3fcb28c790093fe2a7b66009fb7
dalonng <dalonng@gmail.com>
2014-03-07 10:55:07 +0800

    Create picturename.sh

---
+++ picturename.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+counter=1
+root=mypict
+resolution=400x300
+for i in `ls -1 $1/*.jpg`; do
+echo "Now working on $i"
+convert -resize $resolution $i ${root}_${counter}.jpg
+counter=`expr $counter + 1`
+done
